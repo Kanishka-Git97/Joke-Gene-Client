8c75ba6e5e31a1109f0c759a3a9c5f89551c9190
Purna Kanishka <81583629+Kanishka-Git97@users.noreply.github.com>
2023-07-26 15:38:15 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo 'run application_start.sh: ' >> /home/ec2-user/development/Joke-Gebe-Client/deploy.log
+echo 'run application_start.sh: ' >> /home/ec2-user/development/Joke-Gene-Client/deploy.log
 
-echo 'pm2 restart client' >> /home/ec2-user/development/Joke-Gebe-Client/deploy.log
-sudo pm2 restart client >> /home/ec2-user/development/Joke-Gebe-Client/deploy.log
-sudo pm2 save >> /home/ec2-user/development/Joke-Gebe-Client/deploy.log
+echo 'pm2 restart client' >> /home/ec2-user/development/Joke-Gene-Client/deploy.log
+sudo pm2 restart client >> /home/ec2-user/development/Joke-Gene-Client/deploy.log
+sudo pm2 save >> /home/ec2-user/development/Joke-Gene-Client/deploy.log
 
