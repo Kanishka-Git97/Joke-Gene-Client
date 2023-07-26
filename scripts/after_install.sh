833cc75c9acd37723532be229d3d603f802bd0d2
Purna Kanishka <81583629+Kanishka-Git97@users.noreply.github.com>
2023-07-26 15:28:33 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo 'run after_install.sh: ' >> /home/ec2-user/buyasia/buy-asia/deploy.log
+echo 'run after_install.sh: ' >> /home/ec2-user/development/Joke-Gene-Client/deploy.log
 
-echo 'cd /home/ec2-user/development' >> /home/ec2-user/buyasia/buy-asia/deploy.log
-cd /home/ec2-user/buyasia/buy-asia >> /home/ec2-user/buyasia/buy-asia/deploy.log
+echo 'cd /home/ec2-user/development' >> /home/ec2-user/development/Joke-Gene-Client/deploy.log
+cd /home/ec2-user/development/Joke-Gene-Client/ >> /home/ec2-user/development/Joke-Gene-Client/deploy.log
 
-echo 'npm install' >> /home/ec2-user/buyasia/buy-asia/deploy.log 
-npm i --legacy-peer-deps >> /home/ec2-user/buyasia/buy-asia/deploy.log
+echo 'npm install' >> /home/ec2-user/development/Joke-Gene-Client/deploy.log
+npm i --legacy-peer-deps >> /home/ec2-user/development/Joke-Gene-Client/deploy.log
