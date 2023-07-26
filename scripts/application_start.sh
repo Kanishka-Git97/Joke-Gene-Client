5389ce66cc7d00435acc02bd5879e712ba0f793f
Purna Kanishka <81583629+Kanishka-Git97@users.noreply.github.com>
2023-07-26 15:32:05 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo 'run application_start.sh: ' >> /home/ec2-user/buyasia/buy-asia/deploy.log
+echo 'run application_start.sh: ' >> /home/ec2-user/development/Joke-Gebe-Client/deploy.log
 
-echo 'pm2 restart buy-asia' >> /home/ec2-user/buyasia/buy-asia/deploy.log
-sudo pm2 restart buy-asia >> /home/ec2-user/buyasia/buy-asia/deploy.log
-sudo pm2 save >> /home/ec2-user/buyasia/buy-asia/deploy.log
+echo 'pm2 restart client' >> /home/ec2-user/development/Joke-Gebe-Client/deploy.log
+sudo pm2 restart client >> /home/ec2-user/development/Joke-Gebe-Client/deploy.log
+sudo pm2 save >> /home/ec2-user/development/Joke-Gebe-Client/deploy.log
 
